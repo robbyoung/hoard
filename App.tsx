019a7660442bdbc3b3ff738ff2c4d3da2ed409d8
Robert Young <robertyoung10101@gmail.com>
2019-04-06 18:47:46 +1300

    Initialised project as a hello world app

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {}
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello, World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
