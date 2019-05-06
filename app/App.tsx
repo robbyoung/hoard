59852c12490dca1f0655927e7d9f2db83ac96616
Robert Young <robertyoung10101@gmail.com>
2019-05-06 20:57:33 +1200

    Moved App.tsx into the app folder

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import Overview from './app/views/overview';
+import Overview from './views/overview';
 import {View} from 'react-native';
 
 interface Props {}
-export default class App extends Component<Props> {
+export class App extends Component<Props> {
   render() {
 	return (
 		<View>
---
+++ app/views/item.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Item extends Component {
+	public render(): JSX.Element {
+		return (
+			<View>
+				<Text>Item Screen</Text>
+			</View>
+		);
+	}
+}
