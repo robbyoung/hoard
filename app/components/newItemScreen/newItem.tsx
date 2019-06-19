3394796fce0c99e03cfd1fdae4d40c3b29f32da1
Robert Young <robertyoung10101@gmail.com>
2019-06-19 20:14:58 +1200

    Made a simple category picker

--- app/components/newItemScreen/newItem.tsx
+++ app/components/newItemScreen/newItem.tsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Picker, Text } from 'react-native';
 import { NavigationInjectedProps } from 'react-navigation';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: 'row',
+	},
+	categoryPicker: {
+		width: 100,
+		height: 20,
+	}
+});
 
 export default class NewItem extends Component<NavigationInjectedProps> {
 	public static navigationOptions = {
@@ -10,6 +18,18 @@ export default class NewItem extends Component<NavigationInjectedProps> {
 	};
 
 	public render(): JSX.Element {
-		return <View />;
+		return (
+			<View>
+				<View style={styles.row}>
+					<Text>Category:</Text>
+					<Picker style={styles.categoryPicker}
+						selectedValue='Book'>
+						<Picker.Item label="Book" value="Book"></Picker.Item>
+						<Picker.Item label="Game" value="Game"></Picker.Item>
+					</Picker>
+				</View>
+
+			</View>
+		);
 	}
 }
--- app/components/overviewScreen/overview.tsx
+++ app/components/overviewScreen/overview.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import { testInventory } from '../../data/testInventory';
 import OverviewItem from './overviewItem';
 import {
